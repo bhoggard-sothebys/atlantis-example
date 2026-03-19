c1e2d38867f3cae388069b7aa0976397be2e4b09
Barry Hoggard <barry.hoggard@sothebys.com>
2026-03-19 15:41:30 -0400

    test running a script as part of plan step

---
+++ scripts/print-plan-dir.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Prints the directory Atlantis is using for the plan step.
+# Atlantis sets several environment variables during workflow steps:
+#   DIR - absolute path to the directory where Atlantis runs the plan
+#   See https://www.runatlantis.io/docs/custom-workflows.html#reference
+
+echo "Atlantis plan directory: ${DIR}"
